994b1db1f8256992e382fcee44f8f3b824be1de2
Rishabh <rishabh.rstgi06@gmail.com>
2025-11-15 11:12:26 +0000

    added data_mux

---
+++ Red/Data_mux.sv
@@ -0,0 +1,12 @@
+module Data_mux (
+    input logic[31:0] ReadData,
+    input logic[31:0] ALUResult,
+    input logic ResultSrc,
+    output logic Result
+);
+
+
+    assign Result = (ResultSrc)? ReadData: ALUResult;
+    
+    
+endmodule
