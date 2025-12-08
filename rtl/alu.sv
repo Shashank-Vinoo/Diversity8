61042679d4e18d44cf75bcab526931923be63bc4
Arkit28 <93474772+Arkit28@users.noreply.github.com>
2025-12-08 19:35:41 +0000

    Implemented mutiplication and division operations in ALU

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -16,6 +16,14 @@ module alu(
             4'b0011: alu_out = alu_op1 | alu_op2;
             4'b0010: alu_out = alu_op1 & alu_op2;
             // Add Multiply and divide operations according to the alu control signal
+            4'b1000: alu_out = alu_op1[15:0] * alu_op2[15:0]; // lower 16 bits multiplication
+            4'b1001: alu_out = alu_op1[31:16] * alu_op2[31:16]; // upper 16 bits multiplication
+            4'b1010: alu_out = alu_op1[31:16] * alu_op2[31:16];
+            4'b1011: alu_out = alu_op1[31:16] * alu_op2[31:16];
+            4'b1100: alu_out = alu_op1 / alu_op2;
+            4'b1101: alu_out = alu_op1 / alu_op2;
+            4'b1110: alu_out = alu_op1 % alu_op2;
+            4'b1111: alu_out = alu_op1 % alu_op2;
             default: alu_out = 32'b0;
         endcase
    end
