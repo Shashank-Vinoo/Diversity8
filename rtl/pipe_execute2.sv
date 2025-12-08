6922b7a3e361759b30aae2a4953640d60e6bd976
ryang <ryang2856@gmail.com>
2025-12-08 21:59:07 +0000

    Finished implementing deep pipelining

--- rtl/pipe_execute2.sv
+++ rtl/pipe_execute2.sv
@@ -4,23 +4,14 @@ module pipe_execute2(
     
     input logic        reg_write_e2,
     input logic [1:0]  result_src_e2,
-    input logic        mem_read_e2,
     input logic        mem_write_e2,
     input logic [31:0] alu_result_e2,    
     input logic [31:0] write_data_e2,    
     input logic [4:0]  rd_e2,
-    input logic [31:0] pc_e2,
     input logic [31:0] pc_plus4_e2,
-    input logic        jump_e2,
-    input logic [1:0]  branch_e2,
-    input logic        alu_zero_e2,      
-    input logic [31:0] branch_pc_e2,     
-    input logic        pred_taken_e2,
-    input logic [31:0] pred_pc_e2,
     
     output logic        reg_write_m1,
     output logic [1:0]  result_src_m1,
-    output logic        mem_read_m1,
     output logic        mem_write_m1,
     output logic [31:0] alu_result_m1,
     output logic [31:0] write_data_m1,
@@ -28,11 +19,10 @@ module pipe_execute2(
     output logic [31:0] pc_plus4_m1
 );
 
-    always_ff @(posedge clk or posedge rst) begin
+    always_ff @(posedge clk) begin
         if (rst) begin
             reg_write_m1   <= 1'b0;
             result_src_m1  <= 2'b0;
-            mem_read_m1    <= 1'b0;
             mem_write_m1   <= 1'b0;
             alu_result_m1  <= 32'b0;
             write_data_m1  <= 32'b0;
@@ -41,7 +31,6 @@ module pipe_execute2(
         end else begin
             reg_write_m1   <= reg_write_e2;
             result_src_m1  <= result_src_e2;
-            mem_read_m1    <= mem_read_e2;
             mem_write_m1   <= mem_write_e2;
             alu_result_m1  <= alu_result_e2;
             write_data_m1  <= write_data_e2;
